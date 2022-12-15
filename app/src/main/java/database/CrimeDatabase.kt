1274392f32c5bf73b82e42e246e9bffcb73f4591
Gelizpa <gerliz2001@mail.ru>
2022-12-16 01:50:44 +0700

    Создание класса базы данных

---
+++ app/src/main/java/database/CrimeDatabase.kt
@@ -0,0 +1,8 @@
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.bignerdranch.android.criminalintent.Crime
+
+@Database(entities = [ Crime::class ],
+    version=1)
+abstract class CrimeDatabase : RoomDatabase() {
+}
