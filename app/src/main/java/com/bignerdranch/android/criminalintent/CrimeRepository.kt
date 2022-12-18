00c939a4b25ebadbb1121b7bbe8f8091637a4cf1
Gelizpa <gerliz2001@mail.ru>
2022-12-19 00:13:38 +0700

    Использование исполнителя

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -5,6 +5,7 @@ import database.CrimeDatabase
 import android.content.Context
 import androidx.lifecycle.LiveData
 import java.util.*
+import java.util.concurrent.Executors
 
 
 private const val DATABASE_NAME = "crime-database"
@@ -15,9 +16,22 @@ class CrimeRepository private constructor(context: android.content.Context) {
     private val database : CrimeDatabase = Room.databaseBuilder(context.applicationContext,
         CrimeDatabase::class.java, DATABASE_NAME).build()
     private val crimeDao = database.crimeDao()
+    private val executor = Executors.newSingleThreadExecutor()//Функция newSingleThreadExecutor() возвращает экземпляр исполнителя, который указывает на новый поток.
+    //Таким образом, любая работа, которую вы выполняете сисполнителем, будет происходить вне основного потока.
 
     fun getCrimes(): LiveData<List<Crime>> = crimeDao.getCrimes()
     fun getCrime(id: UUID): LiveData<Crime?> = crimeDao.getCrime(id)
+    fun updateCrime(crime: Crime) {
+        executor.execute {
+            crimeDao.updateCrime(crime)
+        }// execute{}. Он выталкивает эти операции из основного потока, чтобы не блокировать работу  пользовательского интерфейса.
+
+    }
+    fun addCrime(crime: Crime) {
+        executor.execute {
+            crimeDao.addCrime(crime)
+        }
+    }
 
     companion object {
         private var INSTANCE: CrimeRepository? = null
