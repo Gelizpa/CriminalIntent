313aad37321c1cb63e1b1f0586c5f9216982b46f
Gelizpa <gerliz2001@mail.ru>
2022-12-19 00:09:38 +0700

    Обновление базы данных

--- app/src/main/java/database/CrimeDao.kt
+++ app/src/main/java/database/CrimeDao.kt
@@ -2,7 +2,9 @@ package database
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.bignerdranch.android.criminalintent.Crime
 import java.util.*
 
@@ -15,6 +17,9 @@ interface CrimeDao {
     @Query("SELECT * FROM crime WHERE id=(:id)")// Команда SELECTFROMcrimeWHEREid=(:id) запрашивает все столбцы только из строки с нужным  идентификатором.
 
     fun getCrime(id: UUID): LiveData<Crime?>//Возвращая экземпляр LiveData из вашего класса DAO, вызапускаете запрос в фоновом потоке. Когда запрос завершается,объект LiveData будет обрабатывать отправку данных преступлений в основной поток и сообщать о любых наблюдателях
-
+    @Update
+    fun updateCrime(crime: Crime)//В функции updateCrime() используется аннотация @Update. Эта функция принимает объект преступления, используя идентификатор, сохраненный в этом преступлении, чтобы найти соответствующую строку, а затем обновляет данные в этой строке, основываясь на новых данных в объекте преступления
+    @Insert
+    fun addCrime(crime: Crime)//В функции addCrime() используется аннотация @Insert. Параметр — это преступление, которое вы хотите добавить в таблицу базы данных
 }
 
