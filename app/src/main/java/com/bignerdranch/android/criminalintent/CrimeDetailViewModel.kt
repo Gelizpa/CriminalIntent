8fe9663603c60c0577983c4b0210db443ec813ad
Gelizpa <gerliz2001@mail.ru>
2022-12-19 00:06:43 +0700

    Преобразования LiveData

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailViewModel.kt
@@ -0,0 +1,21 @@
+package com.bignerdranch.android.criminalintent
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import java.util.*
+
+class CrimeDetailViewModel() : ViewModel() {
+    private val crimeRepository = CrimeRepository.get()//В свойстве CrimeRepository хранится связь с CrimeRepository
+    private val crimeIdLiveData = MutableLiveData<UUID>()//CrimeIdLiveData хранит идентификатор отображаемого в данный момент преступления (или выводимого на отображение) фрагментом CrimeFragment
+    var crimeLiveData: LiveData<Crime?> =
+        Transformations.switchMap(crimeIdLiveData) {
+                crimeId -> crimeRepository.getCrime(crimeId)
+        }//Функция преобразования возвращает новый объект LiveData, который мы называем результатом преобразования, значение которого обновляется каждый раз, когда изменяется значение триггерного объекта LiveData.
+
+    fun loadCrime(crimeId: UUID) {
+        crimeIdLiveData.value = crimeId
+    }//Свойство value объекта LiveData, возвращаемое из функции отображения, используется для установки свойства value для результата преобразования.
+
+}
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -11,9 +11,12 @@ import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.bignerdranch.android.criminalintent.Crime
+import com.bignerdranch.android.criminalintent.CrimeDetailViewModel
 import com.bignerdranch.android.criminalintent.R
 import java.util.*
+import androidx.lifecycle.Observer
 private const val TAG = "CrimeFragment"
 private const val ARG_CRIME_ID = "crime_id"
 class CrimeFragment : Fragment() {
@@ -21,13 +24,18 @@ class CrimeFragment : Fragment() {
     private lateinit var titleField: EditText
     private lateinit var dateButton: Button
     private lateinit var solvedCheckBox: CheckBox
+    private val crimeDetailViewModel: CrimeDetailViewModel by lazy {
+        ViewModelProviders.of(this).get(CrimeDetailViewModel::class.java)
+    }//Запрос у ViewModel загрузку Crime в onCreate(...).
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
         val crimeId: UUID = arguments?.getSerializable(ARG_CRIME_ID) as UUID
-        Log.d(TAG, "args bundle crime ID:$crimeId")
-        // Загрузка преступления из базы данных
+        crimeDetailViewModel.loadCrime(crimeId)
+
     }
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,6 +53,18 @@ class CrimeFragment : Fragment() {
 
         return view
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        crimeDetailViewModel.crimeLiveData.observe(
+            viewLifecycleOwner,
+            Observer { crime ->
+                crime?.let {
+                    this.crime = crime
+                    updateUI()
+                }
+            })
+    }
+
     override fun onStart() {
         super.onStart()
         val titleWatcher = object : TextWatcher
@@ -76,6 +96,16 @@ class CrimeFragment : Fragment() {
         }
 
     }
+    private fun updateUI() {
+        titleField.setText(crime.title)
+        dateButton.text = crime.date.toString()
+        solvedCheckBox.apply {
+            isChecked = crime.isSolved
+            jumpDrawablesToCurrentState()
+        }
+
+    }
+
     companion object {
         fun newInstance(crimeId: UUID): CrimeFragment {
             val args = Bundle().apply {
